95b67470843ec07924bb0beeb61ba2260f1b8dcb
Joona Tiinanen <joona_tiinanen@hotmail.com>
2018-09-29 12:56:32 +0300

    Refactored playercontrollers

---
+++ Assets/Scripts/Controllers/Player1Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : BasePlayerController
+{
+    // Pineapple ability stuff
+
+    // Ability Cooldowns
+    public float pineappleCooldown;
+    public float dashCooldown;
+    public float throwCooldown;
+
+    private enum Ability
+    {
+        Pineapple,
+        Dash,
+        Throw,
+    }
+
+    public override int Health
+    {
+        set
+        {
+            base.Health = value;
+
+            // Play hurt sound etc.
+
+            // Handle death here?
+        }
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
