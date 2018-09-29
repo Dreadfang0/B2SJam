20976b1685ab27e42e8fd00ae3387fe04bb88fee
Joona Tiinanen <joona_tiinanen@hotmail.com>
2018-09-29 14:55:12 +0300

    Added UI controller

---
+++ Assets/Scripts/Singletons/VersusUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersusUI : MonoBehaviour
+{
+    public static VersusUI instance;
+
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(instance.gameObject);
+
+        instance = this;
+    }
+}
